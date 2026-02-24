7b29c248128ec3be25289e7e73451de950b0938b
Matheus Norjosa <117673804+matheusnorjosa@users.noreply.github.com>
2026-02-24 13:57:26 -0300

    fix(frontend-ci): load lighthouse config as cjs in ESM project

--- v2/frontend/eslint.config.js
+++ v2/frontend/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'lighthouserc.js']),
+  globalIgnores(['dist', 'lighthouserc.cjs']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
